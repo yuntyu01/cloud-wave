259dc213b98df2a26d748eb4deed014d005e4b28
yuntyu01 <yuntyu01@gmail.com>
2025-07-24 08:38:13 +0000

    update main page

--- scripts/stop_streamlit.sh
+++ scripts/stop_streamlit.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-# APP_Name=streamlit
 APP_PID=$(pgrep streamlit)
 
-if [ -z $APP_PID ]
-then
+if [ -z "$APP_PID" ]; then
   echo "Application is not running"
 else
   echo "Kill -9 $APP_PID"
-  kill -9 $APP_PID
+  # 모든 프로세스에 대해 kill 실행
+  for PID in $APP_PID
+  do
+    kill -9 $PID
+  done
   sleep 5
 fi
 
